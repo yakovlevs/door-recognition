e355c42a2c2460ea5c488327f35d54362cdf89a8
Sergey Yakovlev <yakovelv.sergey@outlook.com>
2018-04-12 22:46:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import sys
+
+imagePath = sys.argv[1]
+cascadePath = sys.argv[2]
+
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow("Face found", image)
+cv2.waitKey(0)
